4e71ffb39fcf817bed038a37efd8b8e65551fadf
Home <hirotaka.i@gmail.com>
2019-09-22 21:59:42 -0400

    0922

--- submitting.R
+++ submitting.R
@@ -26,13 +26,13 @@ getOne = function(df, item, item_name){
 }
 
 listFunc[[2]] = function(df){
- # get VAS
- getOne(df, '痛みの評価VAS', 'vas') 
+  # get VAS
+  getOne(df, '痛みの評価VAS', 'vas') 
 }
 
 listFunc[[3]] = function(df){
- # get BDI
- getOne(df, "BDI合計点数", 'bdi') 
+  # get BDI
+  getOne(df, "BDI合計点数", 'bdi') 
 }
 
 listFunc[[4]] = function(df){
@@ -40,6 +40,9 @@ listFunc[[4]] = function(df){
   d = bind_cols(
     getOne(df, "痛みの評価合計点数", 'sfpain'),
     getOne(df, "痛みの評価痛みの現在の強さ", 'sfpain_intense'))
+  d1 = df[, grep('痛みの評価No', names(df))]
+  d$sfpainSens = rowSums(d1[,1:11], na.rm = T)
+  d$sfpainAffe = rowSums(d1[,12:15], na.rm = T)
   return(d)
 }
 
@@ -51,7 +54,7 @@ listFunc[[5]] = function(df){
     getOne(df, "UPDRS合計得点", 'updrs3'))
   return(d)
 }
-  
+
 
 listFunc[[6]] = function(df){
   # get PDQ
@@ -59,6 +62,14 @@ listFunc[[6]] = function(df){
           'pdq_social', 'pdq_cog', 'pdq_commu', 'pdq_body')
   dfpdq = df[, grep('PDQ ', names(df))]
   names(dfpdq) = PDQ
+  dfpdq$pdq_mobility = dfpdq$pdq_mobility/40*100
+  dfpdq$pdq_adl = dfpdq$pdq_adl/24*100
+  dfpdq$pdq_ewb = dfpdq$pdq_ewb/24*100
+  dfpdq$pdq_stigma = dfpdq$pdq_stigma/16*100
+  dfpdq$pdq_social = dfpdq$pdq_social/12*100
+  dfpdq$pdq_cog = dfpdq$pdq_cog/12*100
+  dfpdq$pdq_commu = dfpdq$pdq_commu/12*100
+  dfpdq$pdq_body = dfpdq$pdq_body/12*100
   return(dfpdq)
 }
 
@@ -114,12 +125,12 @@ assign = assign %>%
 
 # Baseline
 dfbl = getReady(read_csv('data/CymbaltaTrial_Patient.csv', na = c("@", ""),
-                  locale = (locale(encoding = 'cp932'))), 
-         list_of_func = listFunc[c(1,3:9)], 
-         extraVarNames = c('id', 'date_motor', 'pddrug_effective', 
-                           'painpart', 'painpart2', 
-                           'date_pain', 'height', 'bw'), 
-         extraVarColumns = c(1, 13:19))
+                         locale = (locale(encoding = 'cp932'))), 
+                list_of_func = listFunc[c(1,3:9)], 
+                extraVarNames = c('id', 'date_motor', 'pddrug_effective', 
+                                  'painpart', 'painpart2', 
+                                  'date_pain', 'height', 'bw'), 
+                extraVarColumns = c(1, 13:19))
 dfbl = left_join(assign, dfbl, by = 'id') # basic parameters plus vas is added. 
 dfbl$EVENT='BL'
 
@@ -130,7 +141,7 @@ assign$date_visit = NULL
 
 # Week2
 dfw2 = getReady(read_csv('data/CymbaltaTrial_FollowW2.csv', na = c("@", ""),
-                   locale = (locale(encoding = 'cp932'))),
+                         locale = (locale(encoding = 'cp932'))),
                 list_of_func = listFunc[c(1,9)],
                 extraVarNames = c('id', 'date_visit', 'bw'),
                 extraVarColumns = c(1,7,9))
@@ -163,10 +174,10 @@ dfw12$EVENT='W12'
 
 # drop observation 
 dfdrop = getReady(read_csv('data/CymbaltaTrial_Cancel.csv', na = c("@", ""),
-                          locale = (locale(encoding = 'cp932'))),
-                 list_of_func = listFunc,
-                 extraVarNames = c('id', 'date_visit', 'bw', 'compliance'),
-                 extraVarColumns = c(1,7,9, 19))
+                           locale = (locale(encoding = 'cp932'))),
+                  list_of_func = listFunc,
+                  extraVarNames = c('id', 'date_visit', 'bw', 'compliance'),
+                  extraVarColumns = c(1,7,9, 19))
 dfdrop = dfdrop %>% filter(!is.na(compliance)) %>%
   left_join(., assign, by = 'id')
 dfdrop$EVENT='Drop'
@@ -204,6 +215,7 @@ t = df %>%
     pain_Head=case_when(
       grepl('顔|頭', painpart) | grepl('顔|頭', painpart2) ~ 1,
       TRUE~0)) %>%
+  mutate_at(vars(starts_with('pain_')), as.factor) %>%
   # recode sfpain_intense
   mutate(sfpain_intense = case_when(
     sfpain_intense=='わずかな痛み'~ '1:Mild',
@@ -220,41 +232,194 @@ t = df %>%
     dropI = ifelse(id %in% dfdrop$id, 1, 0) %>% as.factor) %>%
   ungroup() %>% 
   arrange(id, date_visit)
-  
+
 t$led = rowSums(t[pddrug], na.rm = T)
 t[c(pddrug, 'painpart','painpart2', 'comments', 'height', 'bw', 'date_motor', 'date_pain', 'compliance')]=NULL
 
 
+# Create PPS and FAS
+names(t)
+outcomes = names(t)[c(6,8,9,11,12,14:23)]
+units = c('mm', 'Score', 'PRI', 'PRI', 'PRI', 'Score',
+          rep('%', 8), 'seconds')
+testnames = c('Visual Analogue Scale', "Beck's Depression Inventory", 
+              'SF_MPQ, Total', 'SF-MPQ, Sensory', 'SF-MPQ, Affective',
+              'MDS-UPDRS part III', 
+              paste('PDQ39', c('Mobility', 'ADL', 'Emotional Well-Being',
+                               'Stigma', 'Social Support', 'Cognition', 
+                               'Communication', 'Bodily Discomfort'), sep=', '),
+              '3m Up and Go Test') 
+pps = t %>% filter(dropI==0, EVENT %in% c('BL', 'W10'))
+
+fas = t %>% 
+  filter(EVENT!='W12') %>%
+  arrange(id, date_visit) %>%
+  group_by(id) %>%
+  mutate_at(vars(outcomes), na.locf0) %>%
+  mutate(V = 1:n()) %>%
+  mutate(idx = (V==min(V)) | (V==max(V))) %>%
+  ungroup() %>%
+  filter(idx) %>%
+  select(names(pps))
+
+fas %>% with(table(EVENT))
+
+
 # Table 1
 library(tableone)
-t %>% filter(EVENT=='BL') %>%
+fas %>% filter(EVENT=='BL') %>%
   select(-id) %>%
-  CreateTableOne(data= ., strata = 'grp')
+  select(-date_visit) %>%
+  CreateTableOne(data= ., strata = 'grp') %>%
+  print(quote = FALSE, noSpaces = TRUE, printToggle = FALSE) %>%
+  write.csv(file='output/table1.csv')
 
-# Analysis
-# Torelance
-fisher.test(matrix(c(21,2,16,7), ncol=2))
+# Table 2
+dfdrop %>% select(id, compliance, grp)
+
+# Fisher's exact test
+fisher.test(matrix(c(23-2,2,23-7,7), ncol=2))
+
+# SupTable for adverse event
+getAE = function(df){
+  # get pd status
+  AEs = c('id', 'input', 'Dstart', 'status', 'Dend', 'Dcontin', 'contStatus', 'diagnosis',
+          'PD', 'pain', 'lab', 'diagnosisOth', 'seriousness', 'severity', 'causality', 
+          'drugAction', 'otherAction', 'detail')
+  d1 = df[,c(1, grep('有害事象\\[No.1\\]', names(df)))]
+  d2 = df[,c(1, grep('有害事象\\[No.2\\]', names(df)))]
+  d3 = df[,c(1, grep('有害事象\\[No.3\\]', names(df)))]
+  d4 = df[,c(1, grep('有害事象\\[No.4\\]', names(df)))]
+  d5 = df[,c(1, grep('有害事象\\[No.5\\]', names(df)))]
+  names(d1) = AEs
+  names(d2) = AEs
+  names(d3) = AEs
+  names(d4) = AEs
+  names(d5) = AEs
+  return(bind_rows(d1,d2,d3,d4,d5))
+}
+dfae1 = getAE(read_csv('data/CymbaltaTrial_Adverse.csv', na = c("@", ""),
+                       locale = (locale(encoding = 'cp932')))) %>%
+  filter(input!='未入力') %>% 
+  left_join(., assign %>% select(id, grp), by = 'id')
+dfae2 = dfae1 %>%
+  mutate(event = ifelse(diagnosis=='その他', diagnosisOth, diagnosis)) %>%
+  group_by(grp, severity, event) %>%
+  mutate(N = n()) %>%
+  distinct(grp, severity, event, N) %>%
+  arrange(grp, severity, desc(N))
+dfae2 %>% write_csv('output/SupTable1.csv')
+
+
+# Efficacy Analysis
 # VAS
 testfunc = function(df, test_item, cov_items=c()){
-  d1 = df %>% filter(EVENT=='BL') %>%
-    mutate(duloxetine = ifelse(grp=='duloxetine', 1, 0))
-  d2 = df %>% filter(EVENT=='W10')
-  d3 = df %>% filter(EVENT=='Drop')
-  covs=''
+  # if cov_items are given, 
+  # the model would be regressed by the baseline cov_items' values
+  d = df %>% mutate(duloxetine=ifelse(grp=='duloxetine', 1, 0))
+  d[, 'outcome'] = d[, test_item]
+  d = d %>% 
+    arrange(id, date_visit) %>%
+    group_by(id) %>%
+    mutate(change = last(outcome) - first(outcome)) %>% 
+    distinct(id, .keep_all = T)
   if(length(cov_items)>0){
-    covs = paste0(paste(cov_items, collapse ='.y+'), '.y+')
-  }
-  # d3 = data.frame()
-  d = inner_join(d1, bind_rows(d2, d3), by = 'id')
-  m = paste0(test_item, '.y ~ ', covs, 'duloxetine + ', test_item, '.x')
-  print(m)
+    covs = paste0('+', paste(cov_items, collapse ='+'))
+  }else{covs=''}
+  m = paste0('change ~ duloxetine', covs)
   a = lm(m, data = d)
   return(summary(a))
 }
-testfunc(t, 'vas', c('sex', 'age', 'led'))
-testfunc(t, test_item = 'walking', c('BMI'))
-testfunc(t, 'sfpain')
-testfunc(t, 'bdi')
+testfunc(fas, 'vas')
 
-testfunc(t, 'updrs3', c('sex', 'age', 'led'))
+# Table 3
+giveRes = function(df, outcome){
+  t = testfunc(df, outcome)
+  res = coef(t) %>% .['duloxetine',]
+  n = length(t$residuals)
+  b = res[1]
+  se = res[2]
+  l = b - 1.96 * se
+  u = b + 1.96 * se
+  p = res[4]
+  ci =   sprintf('%.1f [%.1f, %.1f]', b,l,u)
+  o = data.frame(outcome = outcome, beta=b, se = se, p = p, ci = ci, n = n)
+  return(o)
+}
 
+lapply(outcomes, function(x){giveRes(fas, x)}) %>%
+  bind_rows() %>%
+  fwrite('output/table3_fas.csv')
+
+
+lapply(outcomes, function(x){giveRes(pps, x)}) %>%
+  bind_rows() %>%
+  fwrite('output/table3_pps.csv')
+
+
+# Figure 1
+pd <- position_dodge(1)
+plotWeek=function(df, outcome){
+  df[, 'outcome'] = df[, outcome]
+  df %>% filter(dropI!=1, EVENT!='W2') %>% 
+    rename(Arm = grp) %>%
+    mutate(Week = case_when(
+      EVENT=='BL'~0,
+      EVENT=='W10'~10,
+      EVENT=='W12'~12
+    )) %>% 
+    filter(!is.na(outcome)) %>%
+    group_by(Week, Arm) %>%
+    do(lm(outcome ~ 1, data=.) %>% broom::tidy()) %>%
+    .[, 1:5] %>%
+    rename(Mean = estimate, SE = std.error) %>%
+    ggplot(aes(x=Week, y = Mean, colour=Arm)) + 
+    geom_line(position=pd, show.legend = FALSE) + 
+    geom_point(position=pd, size=3, show.legend = FALSE) +
+    geom_errorbar(aes(ymin= Mean - 1 * SE, ymax = Mean + 1 * SE), 
+                  width=1, position=pd, show.legend = FALSE) +
+    expand_limits (x=c(0, 12), y=0) + 
+    scale_x_discrete(limits=c(0, 10, 12))+
+    theme_bw() +
+    labs(title = testnames[which(outcomes==outcome)],
+         x = "Week from baseline", y = units[which(outcomes==outcome)])
+}
+plotWeek(fas, outcomes[1])
+figs = list()
+for (i in 1:length(outcomes)){
+  cat(i, outcomes[i], '\n')
+  figs[[i]]=plotWeek(t, outcomes[i])
+}
+p = gridExtra::grid.arrange(grobs=figs[c(1,15,2,3:14)], ncol=5)
+ggsave(plot = p, filename = 'output/fig1.png', width=15, height = 9)
+
+
+
+# SupFig1
+plotFig1=function(df, outcome){
+  d = df %>% 
+    mutate(Week = case_when(
+      EVENT=='BL'~0,
+      EVENT!='BL'~10))
+  d[, 'outcome'] = d[, outcome]
+  # print(d %>% filter(!is.na(outcome)) %>% with(table(grp, EVENT)))
+  ggplot(data=d, aes(y=outcome, x=Week, group = id)) +
+    # geom_boxplot( aes(y=outcome, x=Week)) + 
+    geom_line(show.legend = F, colour='grey') +
+    facet_grid(.~grp) +
+    geom_point(colour='grey') +
+    stat_summary(aes(group = 1), geom = 'line', fun.y = mean,
+                 size = 2, colour='black') +
+    scale_x_discrete(limits=c(0, 10))+
+    theme_bw() +
+    labs(title = testnames[which(outcomes==outcome)],
+         x = "Week from baseline", y = units[which(outcomes==outcome)])
+}
+plotFig1(pps, outcomes[1])
+figs = list()
+for (i in 1:length(outcomes)){
+  cat(i, outcomes[i], '\n')
+  figs[[i]]=plotFig1(fas, outcomes[i])
+}
+p = gridExtra::grid.arrange(grobs=figs, ncol=3, )
+ggsave(plot = p, filename = 'output/supfig1.png', width=9, height = 12)
